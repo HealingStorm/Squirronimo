a9383a6b0342dbd6561eabea63d1bc1510470de4
ErbakAsrevnel <jeanbaptistemaupin@gmail.com>
2021-09-16 02:10:32 +0200

    UI basique fonctionnelle

---
+++ Schirronimo/Assets/Script/UI/UIMenuManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIMenuManager : MonoBehaviour
+{
+    public static UIMenuManager s_Singleton;
+
+    public GameObject pauseMenu;
+
+    public GameObject mainMenu;
+
+    private void Awake() 
+    {
+        if(s_Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            s_Singleton = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void OnPlayPressed()
+    {
+        mainMenu.SetActive(false);
+        SceneManager.LoadScene("Game Scene");
+    }
+
+    public void OnReturnToMenu()
+    {
+        pauseMenu.SetActive(false);
+        mainMenu.SetActive(true);
+        SceneManager.LoadScene("Main Menu Scene");
+    }
+
+    public void OnResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void OnQuitGame()
+    {
+        Application.Quit();
+    }
+
+}
