b7e542d3869267835f0c94af4ef6c3e32cd78c4b
Vincent <vincentpy0902@gmail.com>
2021-09-16 17:17:54 +0200

    début des ennemis qui baissent la velocité du joueur (réduisent son momentum)

---
+++ Schirronimo/Assets/Enemies.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    private Player playerScript;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("gameover");
+            playerScript = other.GetComponent<Player>();
+            playerScript.rb2D.velocity -= new Vector2(0, 5);
+        }
+    }
+}
